ae65a54b60332ab12fead7782c90ae3bf1c2837d
johanna <rodrjoha1.3@gmail.com>
2015-08-07 15:29:08 -0600

    archivo

---
+++ script.R
@@ -0,0 +1,5 @@
+x <- c( 1, 2, 3, 4, 5, 6, 7, 8 )
+y <- c( "juan", "pepe", "iñaky", "amparito"
+        ,"mariano", "juancar", "fulano", "elefante" )
+z <- c( TRUE, TRUE, FALSE, TRUE, FALSE, FALSE, TRUE, TRUE )
+z
